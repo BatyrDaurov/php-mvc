8133f59123eb035eadba7398c81582b90eb6b2ac
BatyrDaurov <batyr.daurov@mail.ru>
2024-12-28 16:16:08 +0300

    закончил формы

--- public/views/login.php
+++ public/views/login.php
@@ -1,13 +1,10 @@
 <h1>Login</h1>
-<form action="" method="post" style="width: 300px; padding: 2em; border: 2px solid #000000; border-radius: 12px">
-    <div class="form-group">
-        <label>Login</label>
-        <input name="login" type="text" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Password</label>
-        <input name="password" type="password" class="form-control">
-    </div>
+<?php
+$form = \app\core\form\Form::begin('', 'post');
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+echo "<div class='col'>" . $form->field($model, 'login') . "</div>";
+echo "<div class='col'>" . $form->field($model, 'password')->passwordField() . "</div>"; ?>
+
+<button type="submit" class="btn btn-primary">Submit</button>
+
+<? \app\core\form\Form::end(); ?>
--- public/views/register.php
+++ public/views/register.php
@@ -1,33 +1,14 @@
 <h1>Create account</h1>
-<form action="" method="post" style="width: 300px; padding: 2em; border: 2px solid red; border-radius: 12px">
-    <div class="row">
-        <div class="col">
-            <div class="form-group">
-                <label>Firstname</label>
-                <input name="firstname" type="text" value=""
-                    class="form-control <?= $model->hasError('firstname') ? 'is-invalid' : '' ?>">
-                <div class="invalid-feedback"><?= $model->getFirstError('firstname'); ?></div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-                <label>Lastname</label>
-                <input name="lastname" type="text" class="form-control">
-            </div>
-        </div>
-    </div>
-    <div class="form-group">
-        <label>Email</label>
-        <input name="email" type="email" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Password</label>
-        <input name="password" type="password" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Confirm Password</label>
-        <input name="passwordConfirm" type="password" class="form-control">
-    </div>
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php
+$form = \app\core\form\Form::begin('', 'post');
+
+echo "<div class='col'>" . $form->field($model, 'firstname') . "</div>";
+echo "<div class='col'>" . $form->field($model, 'lastname') . "</div>";
+echo "<div class='col'>" . $form->field($model, 'email') . "</div>";
+echo "<div class='col'>" . $form->field($model, 'password')->passwordField() . "</div>";
+echo "<div class='col'>" . $form->field($model, 'passwordConfirm')->passwordField() . "</div>"; ?>
+
+<button type="submit" class="btn btn-primary">Submit</button>
+
+<? \app\core\form\Form::end(); ?>
